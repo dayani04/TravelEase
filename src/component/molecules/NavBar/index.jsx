0bdd10977d8fda45bdb924e773ef2d4fce5642b7
dayani04 <dayanisandamali0403@gmail.com>
2023-09-10 15:10:19 +0530

    3rd commit

--- src/component/molecules/NavBar/index.jsx
+++ src/component/molecules/NavBar/index.jsx
@@ -6,7 +6,7 @@ const NavBar=()=>{
           <div class="navbar bg-dark border-bottom border-body"data-bs-theme="dark">
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="#">TravelEase</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>

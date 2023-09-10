cabdd61f4217a3ad45c15ec7836fb0a8dd45b9e4
dayani04 <dayanisandamali0403@gmail.com>
2023-09-10 15:36:51 +0530

    4th Commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Home from './pages/Home';
 import NavItems from './component/atoms/NavItems';
 import NavBar from './component/molecules/NavBar';
-
+import Footer from './component/molecules/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Home/>
-      
+      <Footer/>
     </div>
   );
 }
---
+++ src/component/molecules/Footer/index.jsx
@@ -0,0 +1,16 @@
+const Footer=()=> {
+    return(
+        <div className="footer">
+            <div className="sb_footer section_padding">
+                <div className="sb_footer-links">
+                    <div className="sb_footer-links-div">
+                        <h4>hello world</h4>
+                        
+                    </div>
+                    <h4>privacy policy</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer;

f80840af7ff8009d7142712e47a588719c1cbb82
ZimraMohamed <zimramohamed64@gmail.com>
2023-09-13 14:41:03 +0530

    image Location Second Commit

--- src/pages/Location/index.jsx
+++ src/pages/Location/index.jsx
@@ -2,11 +2,29 @@ import Footer from "../../component/molecules/Footer";
 import NavBar from "../../component/molecules/NavBar";
 
 const Location = () => {
-    const imageUrl = "https://i.pinimg.com/564x/21/19/52/211952d685301f73a1d0e9485b60552e.jpg" // Replace this with your actual image URLhttps://i.pinimg.com/564x/21/19/52/211952d685301f73a1d0e9485b60552e.jpg
+    const imageUrl = "https://www.spendlifetraveling.com/wp-content/uploads/2019/02/must_visit_places_sri_lanka_sigiriya.jpg"
+    const imageUrl1 = "https://www.tourism-of-india.com/blog/wp-content/uploads/2022/03/Colombo-srilanka.png"
+    const imageUrl2 = "https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20180222083159/iStock-531216313.jpg" 
+    const imageUrl3 = "https://www.planetware.com/wpimages/2020/01/sri-lanka-top-attractions-temple-of-the-tooth.jpg"
+    const imageUrl4 = "https://www.bradtguides.com/wp-content/uploads/wysiwyg/destinations/asia/sri-lanka/light-house_galle-fort_sri-lanka.jpg"
+    const imageUrl5 = "https://srilankatravelgram.com/wp-content/uploads/2021/02/Nine-Arches-Bridge.jpg"
+    const imageUrl6 = "https://mediaim.expedia.com/destination/2/531d72bcd95ab28c2fef779182a5da7c.jpg"
+    const imageUrl7 = "https://www.travellankaconnection.com/images/blogs/must-visit-places-in-sri-lanka.jpg"
+    const imageUrl8 = "https://duqjpivknq39s.cloudfront.net/2018/12/polonnaruwa-800x750.jpg"
+    const imageUrl9 = "https://www.lovesrilanka.org/wp-content/uploads/2020/04/Jaffna-Library-1200.jpg"
 
     return (
         <div className="HomePagePicture"> <NavBar/> 
-            <img src={imageUrl} className="img-fluid" alt="Travel" width="800" height="800" />
+            <img src={imageUrl} className="img-fluid" alt="Sigiriya" width="50" height="50" />
+            <img src={imageUrl1} className="img-fluid" alt="Colombo" width="50" height="50" />
+            <img src={imageUrl2} className="img-fluid" alt="Mirissa" width="50" height="50" />
+            <img src={imageUrl3} className="img-fluid" alt="Kandy" width="50" height="50" />
+            <img src={imageUrl4} className="img-fluid" alt="Galle" width="50" height="50" />
+            <img src={imageUrl5} className="img-fluid" alt="Badulla" width="50" height="50" />
+            <img src={imageUrl6} className="img-fluid" alt="Mathara" width="50" height="50" />
+            <img src={imageUrl7} className="img-fluid" alt="Anuradapuraya" width="50" height="50" />
+            <img src={imageUrl8} className="img-fluid" alt="Polonnaruwa" width="50" height="50" />
+            <img src={imageUrl9} className="img-fluid" alt="Jaffna" width="50" height="50" />
             <Footer/>
         </div>
     

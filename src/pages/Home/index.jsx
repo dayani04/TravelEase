9995a50681c06d545a9973ab5aff6935bda024ac
VidminiMinupama <vidminibatuwanthudawa@gmail.com>
2023-09-13 15:05:24 +0530

    Hotel 1st commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,13 @@ import Footer from "../../component/molecules/Footer";
 import NavBar from "../../component/molecules/NavBar";
 
 const Home = () => {
+    console.log("Welcome to our web page");
     const imageUrl = "https://i.pinimg.com/564x/21/19/52/211952d685301f73a1d0e9485b60552e.jpg" // Replace this with your actual image URLhttps://i.pinimg.com/564x/21/19/52/211952d685301f73a1d0e9485b60552e.jpg
-
+    console.log("Welcome to our web page");
     return (
         <div className="HomePagePicture"> <NavBar/> 
-            <img src={imageUrl} className="img-fluid" alt="Travel" width="800" height="800" />
+
+            <img src={imageUrl} className="img-fluid" alt="Travel" width="500" height="500" />
             <Footer/>
         </div>
     

1b54c7e064fe9d207d2d26ec8b245109b9221359
dayani04 <dayanisandamali0403@gmail.com>
2023-09-18 15:32:40 +0530

    02NewLocationbranch 1st commit

--- src/pages/Transpotation/index.jsx
+++ src/pages/Transpotation/index.jsx
@@ -1,55 +1,49 @@
+import NavBar from '../../component/molecules/NavBar';
 import'./index.css';
 
 const Transpotation = () => {
     return (
-        <div>
-        <div className="Location">
-           Hello world
-        </div>
-
-        <div>
-        <div class="card" style={{marginRight: 20 + 'em'}}>
-  <img src="https://www.ohotelsindia.com/pune/images/13c6386862540c41822c39464b130b65.jpg" class="card-img-top" alt="figure1"/>
-  <div class="card-body">
-    <h5 class="card-title"><b>Swimming pool</b></h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">click here</a>
+      <div> <NavBar/> 
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
   </div>
-</div>
-<div class="card" style={{marginRight: 20 + 'em'}}>
-  <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/22/a1/9c/80/essentia-luxury-hotel.jpg?w=700&h=-1&s=1" class="card-img-top" alt="figure1"/>
-  <div class="card-body">
-    <h5 class="card-title"><b>Swimming pool</b></h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">click here</a>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
   </div>
-</div>
-<div class="card" style={{marginRight: 20 + 'em'}}>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM5w_ZcqjcNTVcXKIZwLy7yM707H9MI3y-pw&usqp=CAU" class="card-img-top" alt="figure1"/>
-  <div class="card-body">
-    <h5 class="card-title"><b>Swimming pool</b></h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">click here</a>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
   </div>
-</div>
-<div class="card" style={{marginRight: 20 + 'em'}}>
-  <img src="https://digital.ihg.com/is/image/ihg/ihg-lp-refresh-hero-imea-gben-lvp-1440x617" class="card-img-top" alt="figure1"/>
-  <div class="card-body">
-    <h5 class="card-title"><b>Swimming pool</b></h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">click here</a>
-  </div>
-</div>
-<div class="card" style={{marginRight: 20 + 'em'}}>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHuqtVxwsXC-b_LBDxMQMU27r53_sfUqa5sQ&usqp=CAU" class="card-img-top" alt="figure1"/>
-  <div class="card-body">
-    <h5 class="card-title"><b>Swimming pool</b></h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">click here</a>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
   </div>
 </div>
 </div>
-        </div>
+        
     );
 }
 

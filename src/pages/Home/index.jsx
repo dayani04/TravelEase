b2a5aaa731d7ea994a0f1a4d0f7c4737e4a48987
ZimraMohamed <zimramohamed64@gmail.com>
2023-09-18 09:56:17 +0530

    r

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,14 @@
-
+import Footer from "../../component/molecules/Footer";
+import NavBar from "../../component/molecules/NavBar";
 
 const Home = () => {
     console.log("Welcome to our web page");
     const imageUrl = "https://i.pinimg.com/564x/21/19/52/211952d685301f73a1d0e9485b60552e.jpg" // Replace this with your actual image URLhttps://i.pinimg.com/564x/21/19/52/211952d685301f73a1d0e9485b60552e.jpg
     console.log("Welcome to our web page");
     return (
-        <div className="HomePagePicture"> 
+        <div className="HomePagePicture"> <NavBar/> 
             <img src={imageUrl} className="img-fluid" alt="Travel" width="500" height="500" />
-    
+    <Footer/>
         </div>
     
     );

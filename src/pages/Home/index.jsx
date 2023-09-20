979adf4de233463099c66b99acefccd1e93dc698
ZimraMohamed <zimramohamed64@gmail.com>
2023-09-20 15:10:41 +0530

    new edit hotel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,17 +3,12 @@ import NavBar from "../../component/molecules/NavBar";
 
 const Home = () => {
     console.log("Welcome to our web page");
-    const imageUrl = "https://i.pinimg.com/564x/21/19/52/211952d685301f73a1d0e9485b60552e.jpg" // Replace this with your actual image URLhttps://i.pinimg.com/564x/21/19/52/211952d685301f73a1d0e9485b60552e.jpg
-    console.log("Welcome to our web page");
+    const imageUrl = "https://i.pinimg.com/564x/21/19/52/211952d685301f73a1d0e9485b60552e.jpg" 
     return (
         <div className="HomePagePicture"> <NavBar/> 
-
-
-
-            <img src={imageUrl} className="img-fluid" alt="Travel" width="500" height="500" />
-    <Footer/>
+            <img src={imageUrl} className="img-fluid" alt="Travel" width="500" height="500" />  
+            <Footer />
         </div>
-    
     );
 }
 
--- src/pages/Transpotation/index.jsx
+++ src/pages/Transpotation/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from '../../component/molecules/NavBar';
 import './index.css';
-
+import Footer from "../../component/molecules/Footer";
 const Transportation = () => {
   return (
     <div>
@@ -13,8 +13,7 @@ const Transportation = () => {
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/8/80/2020_Toyota_HiAce_%28front%29.jpg"
               class="card-img-top"
-              alt="..."
-            />
+              alt="..."/>
             <div class="card-body">
               <h2>
                 <b>01</b>
@@ -22,12 +21,7 @@ const Transportation = () => {
               <h4 class="card-title">
                 <b>Van</b>
               </h4>
-              <p class="card-text">
-                This is a Toyota Hiace van, offers spacious seating, versatile
-                cargo capacity, and dependable performance. It's ideal for both
-                passenger transport and commercial use, with a reputation for
-                reliability and durability.
-              </p>
+              <p class="card-text">This is a Toyota Hiace van, offers spacious seating, versatile cargo capacity, and dependable performance. It's ideal for both passenger transport and commercial use, with a reputation for  reliability and durability.</p>
               <p>One Day - $500</p>
             </div>
           </div>
@@ -37,8 +31,7 @@ const Transportation = () => {
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaISuqleHfgfxaAoeepojHEqUFPiSTp5c3Lb4KbJbtHyaPAvMexjbyPCIw6tZdxm_gAm0&usqp=CAU"
               class="card-img-top"
-              alt="..."
-            />
+              alt="..." />
             <div class="card-body">
               <h2>
                 <b>02</b>
@@ -46,11 +39,7 @@ const Transportation = () => {
               <h4 class="card-title">
                 <b>AC Bus</b>
               </h4>
-              <p class="card-text">
-                This is a AC buses, offer climate-controlled comfort with air
-                conditioning, spacious seating, and amenities like Wi-Fi and
-                charging outlets for passenger convenience.
-              </p>
+              <p class="card-text">This is a AC buses, offer climate-controlled comfort with air conditioning, spacious seating, and amenities like Wi-Fi and charging outlets for passenger convenience.</p>
               <p>One Day - $300</p>
             </div>
           </div>
@@ -69,12 +58,7 @@ const Transportation = () => {
               <h4 class="card-title">
                 <b>Car</b>
               </h4>
-              <p class="card-text">
-                This is a Mercedes-Benz cars, offer luxury, advanced
-                technology, and performance. They include features like premium
-                materials, cutting-edge infotainment, safety systems, and a wide
-                range of models catering to various preferences..
-              </p>
+              <p class="card-text">This is a Mercedes-Benz cars, offer luxury, advanced technology, and performance. They include features like premium materials, cutting-edge infotainment, safety systems, and a wide range of models catering to various preferences.</p>
               <p>One Day - $900</p>
             </div>
           </div>
@@ -84,8 +68,7 @@ const Transportation = () => {
             <img
               src="https://i.pinimg.com/736x/bb/a2/ec/bba2ec19ae303d8131039f9cc029b761--war-memorials-water-tower.jpg"
               class="card-img-top"
-              alt="..."
-            />
+              alt="..."/>
             <div class="card-body">
               <h2>
                 <b>04</b>
@@ -94,11 +77,7 @@ const Transportation = () => {
                 <b>Train</b>
               </h4>
               <p class="card-text">
-                This is a Trains, provide efficient, mass transit with
-                comfortable seating, restrooms, and dining options. Many offer
-                Wi-Fi, power outlets, and scenic views, making travel enjoyable
-                and eco-friendly.
-              </p>
+                This is a Trains, provide efficient, mass transit with comfortable seating, restrooms, and dining options. Many offer Wi-Fi, power outlets, and scenic views, making travel enjoyable and eco-friendly.</p>
               <p>One Day - $250</p>
             </div>
           </div>
@@ -108,8 +87,7 @@ const Transportation = () => {
             <img
               src="https://bd.gaadicdn.com/upload/modellogo/64afb0b239cd1.jpg"
               class="card-img-top"
-              alt="..."
-            />
+              alt="..." />
             <div class="card-body">
               <h2>
                 <b>05</b>
@@ -117,12 +95,7 @@ const Transportation = () => {
               <h4 class="card-title">
                 <b>Scooter</b>
               </h4>
-              <p class="card-text">
-                This is a Scooters, compact two-wheelers, perfect for city
-                commuting. They offer fuel efficiency, easy parking, and often
-                include storage compartments. Electric scooters are eco-friendly
-                alternatives gaining popularity.
-              </p>
+              <p class="card-text">This is a Scooters, compact two-wheelers, perfect for city commuting. They offer fuel efficiency, easy parking, and often include storage compartments. Electric scooters are eco-friendly alternatives gaining popularity.</p>
               <p>One Day - $700</p>
             </div>
           </div>
@@ -132,8 +105,7 @@ const Transportation = () => {
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSOm1aqrf7kaQTyg_vSnv6zM3e2Vg2bnaEMxzKyiJiMnqAJHoGmJtgE3dP8crNe1l1ZKQ&usqp=CAU"
               class="card-img-top"
-              alt="..."
-            />
+              alt="..."/>
             <div class="card-body">
               <h2>
                 <b>06</b>
@@ -141,19 +113,12 @@ const Transportation = () => {
               <h4 class="card-title">
                 <b>Travel Jeep</b>
               </h4>
-              <p class="card-text">
-                This is a Travel jeeps, rugged vehicles ideal for off-road
-                adventures. They provide four-wheel drive, durability, and ample
-                seating for outdoor exploration, often equipped with features
-                like roof racks and all-terrain tires.
-              </p>
+              <p class="card-text">This is a Travel jeeps, rugged vehicles ideal for off-road adventures. They provide four-wheel drive, durability, and ample seating for outdoor exploration, often equipped with features like roof racks and all-terrain tires.   </p>
               <p>One Day - $800</p> <br />
             </div>
           </div>
         </div>
       </div>
-
-    
       <form class="row g-3">
         <label for="name">Name:</label>
         <input type="text" id="name" name="name" /><br />
@@ -163,8 +128,13 @@ const Transportation = () => {
         <input type="text" id="Msg" name="Msg" /><br />
         <input type="submit" value="Submit" />
       </form>
+      
+      <Footer/>
     </div>
+
+  
   );
 }
 
 export default Transportation;
+
